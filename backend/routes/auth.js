b9d75b4c9bf2db854f860092985fe5ede4c64cc6
Praful-B <baleraoprafulchandra2006@gmail.com>
2025-09-02 22:24:48 +0530

    save-no-errors

---
+++ backend/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("expresss");
+
+const router = express.router;
+
+router.post("/register", async (req, res) => {
+    
+});
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+
+
+app.use(); // -> /user , user_route_path
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
